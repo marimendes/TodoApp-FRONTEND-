22ec116bc5e5c5e21accf27a6763ba4bf71235c4
Mariana Mendes <mare.s.mendes@gmail.com>
2018-08-09 09:47:39 -0300

    Integração com o backend

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
 	constructor(props) {
 		super(props)
 		this.state = { description: '', list: [] }
-		
+
 		this.handleChange = this.handleChange.bind(this)
 		this.handleAdd = this.handleAdd.bind(this)
 	}
@@ -17,7 +21,9 @@ export default class Todo extends Component {
 	}
 
 	handleAdd() {
-		console.log(this.state.description)
+		const description = this.state.description
+		axios.post(URL, { description })
+			.then(resp => console.log('Funcionou!'))
 	}
 		
 	render() {
